894f3254ea944e6f01e82ce101603b387356b892
Steven Leiva <leiva.steven@gmail.com>
2016-03-19 11:54:58 -0400

    Add ESLint to project.
    - Add eslint and eslint-loader as dev dependencies.
    - Configure webpack to run linter in the preLoaders section.
    - Configure eslint (in webpack) to emit errors and warnings, and fail on
      both.

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,17 @@ module.exports = {
     filename: "build.js"
   },
   module: {
+    preLoaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "eslint-loader" }
+    ],
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
     ]
+  },
+  eslint: {
+    emitError: true,
+    emitWarning: true,
+    failOnError: true,
+    failOnWarning: true
   }
 };
