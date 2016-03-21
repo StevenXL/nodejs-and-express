079558fb65117fb7a1bdfb50cd6a507924823fc9
Steven Leiva <leiva.steven@gmail.com>
2016-03-20 22:57:54 -0400

    Add body-parser middleware.
    - Middleware necessary to parse data in forms, among other uses.

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const nav = [{"Link": "/Books","Text": "Books"}, {"Link": "/Authors", "Text": "Authors"}];
 const bookRouter = require("./src/routes/bookRoutes")(nav);
+const adminRouter = require("./src/routes/adminRoutes")(nav);
+const authRouter = require("./src/routes/authRoutes")(nav);
 
 const app = express();
 const port = 5000;
 
 app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({"extended": true}));
 app.set("views", "./src/views");
-
 app.set("view engine", "ejs");
 
-
 app.use("/Books", bookRouter);
+app.use("/Admin", adminRouter);
+app.use("/Auth", authRouter);
 
 app.get("/", (req, res) => {
-  res.render("index", {
-    "title": "Hello From EJS Render",
-    "nav": [
-      {"Link": "/Books", "Text": "Books"},
-      {"Link": "/Authors", "Text": "Authors"}
-    ]
-  });
+  res.render("index", {"nav": nav});
 });
 
 app.listen(port, (err) => {
